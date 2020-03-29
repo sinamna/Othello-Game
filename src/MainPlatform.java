65514af8160ebe367cf5012a3295f7709ccc47f7
sinamna <sina3050@gmail.com>
2020-03-29 19:05:45 +0430

    mainly done 90% .AI remained and some minor bugs

--- src/MainPlatform.java
+++ src/MainPlatform.java
@@ -5,16 +5,20 @@ public class MainPlatform {
         Player playerTwo=new Player(2);
         boolean turnOne=false;
         while(map.continueCondition()){
-            map.printMap();
+            //map.printMap();
             if(turnOne){
+                map.printMap(1);
                 System.out.println("white's turn: ");
                 map.placeDisc(playerOne);
                 turnOne=false;
             }else{
+                map.printMap(2);
                 System.out.println("black's turn: ");
                 map.placeDisc(playerTwo);
                 turnOne=true;
             }
         }
+        System.out.printf("Player 1: %d discs\nPlayer 2: %d discs\n",playerOne.getDiscCount(),playerTwo.getDiscCount());
+        System.out.printf("%s",playerOne.getDiscCount()>playerTwo.getDiscCount() ? "Player 1 won" : "Player 2 won");
     }
 }
--- src/Map.java
+++ src/Map.java
@@ -15,56 +15,68 @@ public class Map {
         this.placeDisc(1,5-1,convertChar('E'));
         this.placeDisc(2,4-1,convertChar('E'));
         this.placeDisc(2,5-1,convertChar('D'));
-        //x-1 is the position of first dimension of array
-        this.flipDiscs(1,4-1,convertChar('D'));
-        this.flipDiscs(1,5-1,convertChar('E'));
-        this.flipDiscs(2,4-1,convertChar('E'));
-        this.flipDiscs(2,5-1,convertChar('D'));
-
     }
     public void placeDisc(Player player){
         //takes mokhtasat -> checks it can be placed -> new a disc and adds to players disc
         //changes map discs player Id
         Scanner input=new Scanner(System.in);
         // continue taking inputs until user enters correct input.
-        while(true) {
-            int x=Integer.parseInt(input.next());
-            char yChar=input.next().charAt(0);
-            if (canPlace(player, x - 1, convertChar(yChar))) {
-                this.placeDisc(player.getPlayerId(), x - 1, convertChar(yChar));
-                player.setDiscCount(player.getDiscCount() + 1);
-                break;
-            } else {
-                System.out.println("You can not place disc at this place");
+        if(this.placesToChoice(player.getPlayerId())!=0){
+            while(true) {
+                try {
+                    int x=Integer.parseInt(input.next());
+                    char yChar=input.next().charAt(0);
+                    if (checkPlace(player.getPlayerId(), x - 1, convertChar(yChar))) {
+                        this.placeDisc(player.getPlayerId(), x - 1, convertChar(yChar));
+                        player.setDiscCount(player.getDiscCount() + 1);
+                        break;
+                    } else {
+                        System.out.println("You can not place disc at this place");
+                    }
+                }catch (Exception e){
+                    System.out.println("Please enter correct form ");
+                }
+
+
             }
+        }else {
+            System.out.println("pass");
         }
+
     }
+    /**
+     * method goes throw all 8 direction and checks if it can be placed based on opposite compontent discs or not
+     * @param player is the id of player.1 for white disc and 2 for black discs
+     * @param x place in the map
+     * @param y place in the map
+     * @return whether can be placed in (x,y) or not
+     */
     private boolean checkPlace(int player,int x,int y){
         int opPlayer=player==1?2:1;
         if (map[x][y].getPlayer()!=0) return false;
         //horizontal line
         int counter=0;
         for(int i=y;i<8;i++){
-            if(i!=y && map[x][i].getPlayer()==0) return false;
+            if(i!=y && map[x][i].getPlayer()==0)break;
             if(map[x][i].getPlayer()==opPlayer) counter++;
             if(i!=y && map[x][i].getPlayer()==player && counter!=0)return true;
         }
         counter=0;
         for(int i=y;i>=0;i--){
-            if(i!=y && map[x][i].getPlayer()==0) return false;
-            if(map[x][i].getPlayer()==opPlayer) counter++;
+            if(i!=y && map[x][i].getPlayer()==0)break;
+            if(map[x][i].getPlayer()==opPlayer)counter++;
             if(i!=y && map[x][i].getPlayer()==player && counter!=0)return true;
         }
         //vertical line
         counter=0;
         for(int j=x;j>=0;j--){
-            if(j!=x &&map[j][y].getPlayer()==0)return false;
+            if(j!=x &&map[j][y].getPlayer()==0)break;
             if(map[j][y].getPlayer()==opPlayer)counter++;
             if(j!=x && map[j][y].getPlayer()==player && counter!=0)return true;
         }
         counter=0;
         for(int j=x;j<8;j++){
-            if(j!=x &&map[j][y].getPlayer()==0)return false;
+            if(j!=x &&map[j][y].getPlayer()==0)break;
             if(map[j][y].getPlayer()==opPlayer)counter++;
             if(j!=x && map[j][y].getPlayer()==player && counter!=0)return true;
         }
@@ -72,7 +84,7 @@ public class Map {
         counter=0;
         int j=x;
         for(int i=y;i>=0;i--){
-            if(j!=x && i!=y &&map[j][i].getPlayer()==0)return false;
+            if(j!=x && i!=y &&map[j][i].getPlayer()==0)break;
             if(map[j][i].getPlayer()==opPlayer)counter++;
             if(j!=x && i!=y &&map[j][i].getPlayer()==player &&counter!=0)return true;
             if(j-1>=0)
@@ -83,7 +95,7 @@ public class Map {
         counter=0;
         j=x;
         for(int i=y;i<8;i++){
-            if(j!=x && i!=y &&map[j][i].getPlayer()==0)return false;
+            if(j!=x && i!=y &&map[j][i].getPlayer()==0)break;
             if(map[j][i].getPlayer()==opPlayer)counter++;
             if(j!=x && i!=y &&map[j][i].getPlayer()==player &&counter!=0)return true;
             if(j+1<8)
@@ -95,7 +107,7 @@ public class Map {
         j=x;
         counter=0;
         for(int i=y;i>=0;i--){
-            if(j!=x && i!=y &&map[j][i].getPlayer()==0)return false;
+            if(j!=x && i!=y &&map[j][i].getPlayer()==0)break;
             if(map[j][i].getPlayer()==opPlayer)counter++;
             if(j!=x && i!=y &&map[j][i].getPlayer()==player &&counter!=0)return true;
             if(j+1<8)
@@ -106,7 +118,7 @@ public class Map {
         counter=0;
         j=x;
         for(int i=y;i<8;i++){
-            if(j!=x && i!=y &&map[j][i].getPlayer()==0)return false;
+            if(j!=x && i!=y &&map[j][i].getPlayer()==0)break;
             if(map[j][i].getPlayer()==opPlayer)counter++;
             if(j!=x && i!=y &&map[j][i].getPlayer()==player &&counter!=0)return true;
             if(j-1>=0)
@@ -114,21 +126,17 @@ public class Map {
             else
                 break;
         }
-        System.out.println("what?");
         return false;
     }
     private void placeDisc(int playerId,int x,int y){
            map[x][y].setPlayer(playerId);
            this.flipDiscs(playerId,x,y);
-           this.prepareSpace(playerId,x,y);
-          // player.setDiscCount(player.getPlayerId()+1);
     }
     private void flipDiscs(int player,int x,int y){
         /*
         sth about using i and j
         x in multiDimensional array represents j
         and y in multidimensional array represents i
-
          */
         int counter=0;
         int opPlayer= player==1? 2:1;
@@ -140,7 +148,6 @@ public class Map {
                 if(map[x][i].getPlayer()==player){
                     while(counter>0){
                         map[x][y+counter].setPlayer(player);
-                        this.prepareSpace(player,x,y+counter);
                         counter--;
                     }
                     if(i!=y)
@@ -157,7 +164,6 @@ public class Map {
                 if(map[x][i].getPlayer()==player){
                     while(counter>0){
                         map[x][y-counter].setPlayer(player);
-                        this.prepareSpace(player,x,y-counter);
                         counter--;
                     }
                     if(i!=y)
@@ -176,7 +182,6 @@ public class Map {
                 if(map[j][y].getPlayer()==player){
                     while(counter>0){
                         map[x-counter][y].setPlayer(player);
-                        this.prepareSpace(player,x-counter,y);
                         counter--;
                     }
                     if(j!=x)
@@ -193,7 +198,6 @@ public class Map {
                 if(map[j][y].getPlayer()==player){
                     while(counter>0){
                         map[x+counter][y].setPlayer(player);
-                        this.prepareSpace(player,x+counter,y);
                         counter--;
                     }
                     if(j!=x)
@@ -213,7 +217,6 @@ public class Map {
                 if(map[j][i].getPlayer()==player){
                     while(counter>0){
                         map[x-counter][y-counter].setPlayer(player);
-                        this.prepareSpace(player,x-counter,y-counter);
                         counter--;
                     }
                     if(i!=y && j!=x)
@@ -237,8 +240,6 @@ public class Map {
                     //System.out.println(counter);
                     while(counter>0){
                         map[x+counter][y+counter].setPlayer(player);
-
-                        this.prepareSpace(player,x+counter,y+counter);
                         counter--;
                     }
                     if(i!=y && j!=x)
@@ -262,7 +263,6 @@ public class Map {
                 if(map[j][i].getPlayer()==player){
                     while(counter>0){
                         map[x+counter][y-counter].setPlayer(player);
-                        this.prepareSpace(player,x+counter,y-counter);
                         counter--;
                     }
                     if(i!=y && j!=x)
@@ -285,7 +285,6 @@ public class Map {
                 if (map[j][i].getPlayer() == player) {
                     while (counter > 0) {
                         map[x - counter][y + counter].setPlayer(player);
-                        this.prepareSpace(player,x-counter,y+counter);
                         counter--;
                     }
                     if (i != y && j != x)
@@ -300,59 +299,54 @@ public class Map {
                 break;
         }
     }
-    /**
-     * ok ...this method is weird.it checks around specific disc . if it encounter
-     * disc of opposite player mark the opposite situation in a manner which means the other
-     * player can place their disc there
-     * @param player is the Id of player
-     * @param x is the position in first dimension of array
-     * @param y is the position in the second dimension of array
-     */
-    private void prepareSpace(int player,int x,int y){
-        int opPlayer= player==1? 2:1;
-        for(int i=y-1;i<=y+1;i++){
-            for(int j=x-1;j<=x+1;j++){
-                try{
-                    if(map[j][i].getPlayer()!=player && map[j][i].getPlayer()==opPlayer){
-                        if(map[x-(j-x)][y-(i-y)].getPlayer()<=0)
-                            map[x-(j-x)][y-(i-y)].setPlayer(-opPlayer);
-                    }
-                } catch (Exception e){
-                }
-            }
-        }
-    }
-    private boolean canPlace(Player player,int x,int y){
-        return map[x][y].getPlayer()==(-player.getPlayerId());
-    }
-    public void printMap() {
+    public void printMap(int playerId) {
         /*
         player 1 has white discs
         player 2 has black discs
          */
-        for (int i = 0; i < 8; i++) {
-            for (Disc disc : map[i]) {
-                if (disc.getPlayer() == 1) {
-                    System.out.printf("%c  ", 'w');
-                } else if (disc.getPlayer() == 2) {
-                    System.out.printf("%c  ", 'b');
-                }else if(disc.getPlayer()==-1){
-                    System.out.printf("%c  ",'3');
-                }else if(disc.getPlayer()==-2){
-                    System.out.printf("%c  ",'4');
-                }else if(disc.getPlayer()==-3){
-                    System.out.printf("%c  ",'5');
+        System.out.print("   |");
+        for(char c='A';c<='H';c++){
+            System.out.printf("  %c  ",c);
+        }
+        System.out.println("|");
+        System.out.println("---  ---  ---  ---  ---  ---  ---  ---  ---");
+        for(int j=0;j<8;j++){
+            System.out.printf(" %d |",j+1);
+            for(int i=0;i<8;i++){
+                if(map[j][i].getPlayer()==1)
+                    System.out.printf("  %c  ",'\u25cf');
+                else if(map[j][i].getPlayer()==2)
+                    System.out.printf("  %c  ",'\u25cb');
+                else if(this.checkPlace(playerId,j,i)){
+                    if(playerId==1)
+                        System.out.printf(" p%c  ",'W');
+                    else
+                        System.out.printf(" p%c  ",'B');
                 }
-                else {
-                    System.out.printf("0  ");
-                }
+                else
+                    System.out.printf("  .  ");
+
             }
-            System.out.println();
+            System.out.println("\n     ---  ---  ---  ---  ---  ---  ---  --- |");
         }
     }
-
+    /**
+     *
+     * @param player the id of the player
+     * @return how many places avaliable to choose in map for that player
+     */
+    private int placesToChoice(int player){
+        int counter=0;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                if(checkPlace(player,i,j))counter++;
+            }
+        }
+        return counter;
+    }
     public boolean continueCondition(){
-        return true;
+
+        return placesToChoice(1)!=0 && placesToChoice(2)!=0;
     }
     /**
      *
